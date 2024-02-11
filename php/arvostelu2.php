916611ef16ed070a1dd93bc2260b30cfee58ec19
Joonsak <joona22010@student.hamk.fi>
2024-02-11 17:37:34 +0200

    Fixed the navbar and made the reviews show immediately

--- php/arvostelu2.php
+++ php/arvostelu2.php
@@ -1,18 +1,26 @@
 <?php 
-$init=parse_ini_file("../asetus/.ht.asetukset.ini");
-try{
-   $yhteys=mysqli_connect($init["palvelin"], $init["tunnus"], $init["pass"], $init["tk"]);
+$init = parse_ini_file("../asetukset/.ht.asetukset.ini");
+$yhteys = mysqli_connect($init["databaseserver"], $init["username"], $init["password"], $init["database"]);
+
+// Check if the connection was successful
+if (!$yhteys) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-catch(Exception $e){
-    print "Yhteysvirhe";
-    exit;
+
+$tulos = mysqli_query($yhteys, "SELECT * FROM arvostelut");
+
+if ($tulos) { // Check if query was successful
+    print "<div class='arvostelus'>";
+    while ($rivi = mysqli_fetch_object($tulos)) {
+        print "<div>";
+        print "<h2>$rivi->nimimerkki</h2>";
+        print "<p>$rivi->arvostelu</p>";
+        print "</div>"; 
+    }
+    print "</div>"; 
+} else {
+    echo "Error executing query: " . mysqli_error($yhteys);
 }
-$tulos=mysqli_query($yhteys, "select * from arvostelut");
-print "<div class=arvostelus>";
-while ($rivi=mysqli_fetch_object($tulos)){
-print "<div>";
-print "<h2>$rivi->nimimerkki</h2>";
-print"<p> $rivi->arvostelu</p>";
-print"</div>"; 
-}
-print"</div>"; 
+
+mysqli_close($yhteys);
+?>
